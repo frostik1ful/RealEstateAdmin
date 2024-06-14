3bd5f0e85ef5abafee2a4cd28edd456bc5d21fa2
dimatik <D4xGkKpVmr79uhn8mh43>
2024-06-14 14:23:53 +0300

    first commit

---
+++ src/main/java/com/example/real/estate/admin/RealEstateAdmin/RealEstateAdminApplication.java
@@ -0,0 +1,13 @@
+package com.example.real.estate.admin.RealEstateAdmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealEstateAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealEstateAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/real/estate/admin/RealEstateAdmin/RealEstateAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.real.estate.admin.RealEstateAdmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealEstateAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
